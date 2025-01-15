069a4e96a9ac6d9327c0d4e67aad039d1916f103
dakshh-panchal <dakshpanchal08@gmail.com>
2025-01-16 01:37:38 +0530

    Added Most Functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include "raylib.h"
+#include <raylib.h>
 #include <deque>
-#define RAYMATH_IMPLEMENTATION
-#include "raymath.h"
+#include <raymath.h>
 
 using namespace std;
 
@@ -11,6 +10,7 @@ Color darkgreen = {43,51,24,255};
 
 int cellcount = 25;
 int cellsize = 30;
+int score = 0;
 
 class Food
 {
@@ -44,6 +44,17 @@ public:
 
     Vector2 direction = {1,0};
 
+    int len = 3;
+
+    void Eat()
+    {
+        body.push_back(body[len-1]);
+        int x = body[len-1].x;
+        int y = body[len-1].y;
+        Rectangle unit = Rectangle{(float)x*cellsize, (float)y*cellsize, (float)cellsize, (float)cellsize};
+        DrawRectangleRounded(unit, 0.5, 6, darkgreen);
+    }
+
     void Draw()
     {
         for(int i=0; i < body.size(); i++)
@@ -60,6 +71,7 @@ public:
         body.pop_back();
         body.push_front(body[0]+direction);
     }
+
 };
 
 int main()
@@ -79,30 +91,51 @@ int main()
 		Rectangle head = {snake.body[0].x * cellsize, snake.body[0].y * cellsize, (float)cellsize, (float)cellsize};
 		Rectangle f = {food.position.x * cellsize, food.position.y * cellsize,(float)cellsize, (float)cellsize};
 		bool col = CheckCollisionRecs(f , head);
+        // bool colbody = false;
 
+        // for(int i=2;i<snake.len;i++)
+        // {
+        //     Rectangle rec = {snake.body[i].x * cellsize, snake.body[i].y * cellsize, (float)cellsize, (float)cellsize};
+        //     if(CheckCollisionRecs(head,rec))
+        //     {
+        //         colbody = true;
+        //     }
+        // }
 
-		if (col){
-			food.position = food.RandomPosi();
-		}
         BeginDrawing();
+
         ClearBackground(green);
-        food.Draw();
-        snake.Draw();
 
+        if (snake.body[0].x >= 25 || snake.body[0].y >= 25 || snake.body[0].x < 0 || snake.body[0].y < 0)
+        {
+            DrawText("OVER!!",10,10,40,BLACK);
+        }
+        else
+        {
+            if (col)
+            {
+                food.position = food.RandomPosi();
+                snake.Eat();
+                snake.len ++;
+                score++;
+            }
+            food.Draw();
+            snake.Draw();
+            DrawText(TextFormat("%i",score), 5, 5, 25, BLACK);
 
-        if(IsKeyDown(KEY_UP) && snake.direction.y != 1) snake.direction = {0,-1};
+            if(IsKeyDown(KEY_UP) && snake.direction.y != 1) snake.direction = {0,-1};
 
-        if(IsKeyDown(KEY_DOWN) && snake.direction.y != -1) snake.direction = {0,1};
-        
-        if(IsKeyDown(KEY_LEFT) && snake.direction.x != 1) snake.direction = {-1,0};
-
-        if(IsKeyDown(KEY_RIGHT) && snake.direction.x != -1) snake.direction = {1,0};
-        
-        snake.Move();
-
+            if(IsKeyDown(KEY_DOWN) && snake.direction.y != -1) snake.direction = {0,1};
+            
+            if(IsKeyDown(KEY_LEFT) && snake.direction.x != 1) snake.direction = {-1,0};
+            
+            if(IsKeyDown(KEY_RIGHT) && snake.direction.x != -1) snake.direction = {1,0};
+            
+            snake.Move();
+            
+        }
         EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
